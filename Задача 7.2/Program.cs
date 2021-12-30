41c311c267e446f29bd8d65bfca797ec259ad690
mastiaha <mastiaha@mail.ru>
2021-12-30 19:50:58 +0300

    Решение задачи 2

---
+++ Задача 7.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_7._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра куба");
+            int a = Convert.ToInt32(Console.ReadLine());
+            
+            double v = Objom(a);
+            double S = SPoverhnocti(a);
+            Console.WriteLine("Объем куба составляет {0}",v);
+            Console.WriteLine("Площадь общей поверхности куба составляет {0}",S);
+            Console.WriteLine("Для выхода из программы нажмите любую клавишу");
+            Console.ReadKey();
+        }
+        static double Objom (int a)
+        {
+            double v = Math.Pow(a,3);
+            return v;
+        }
+        static double SPoverhnocti(int a)
+        {
+            double S = a * a * 6; // где 6 это количество граней куба
+            return S;
+        }
+    }
+}
